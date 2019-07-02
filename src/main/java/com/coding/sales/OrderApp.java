6ce0f277caf19d155007150658115be233ff6b0d
chenfeng <844474380@qq.com>
2019-07-02 21:51:26 +0800

    添加注释，针对客户使用折扣券 逻辑修改

--- src/main/java/com/coding/sales/OrderApp.java
+++ src/main/java/com/coding/sales/OrderApp.java
@@ -9,7 +9,6 @@ import com.coding.sales.output.DiscountItemRepresentation;
 import com.coding.sales.output.OrderItemRepresentation;
 import com.coding.sales.output.OrderRepresentation;
 import com.coding.sales.output.PaymentRepresentation;
-import com.sun.codemodel.internal.JForEach;
 
 import java.math.BigDecimal;
 import java.text.ParseException;
@@ -46,21 +45,32 @@ public class OrderApp {
     OrderRepresentation checkout(OrderCommand command) {
         OrderRepresentation result = null;
 
-        //TODO: 请完成需求指定的功能
         //初始化产品信息
         Map<String,Object> productInfoMap = initProduct();
-        String orderId = command.getOrderId(); //订单号
-        Date createTime = StringToDate(command.getCreateTime()); //创建时间
-        String memberNo = command.getMemberId(); //客户号
+        //订单号
+        String orderId = command.getOrderId();
+        //创建时间
+        Date createTime = StringToDate(command.getCreateTime());
+        //客户号
+        String memberNo = command.getMemberId();
+        //
         List<OrderItemRepresentation> items = new ArrayList<OrderItemRepresentation>();;
         Member member = (Member) Member.memberMap.get(memberNo);
+        //旧客户等级
         String oldMemberType = member.getMemberType();
-        String memberName = member.getMemberName();//客户姓名
+        //客户姓名
+        String memberName = member.getMemberName();
+        //定义订单总价
         BigDecimal totalPrice = new BigDecimal("0.00");;
-        int oldMemberPoints = member.getMemberPoints(); //购买商品前积分值
+        //购买商品前积分值
+        int oldMemberPoints = member.getMemberPoints();
+        //折扣信息相关list
         List<DiscountItemRepresentation> discounts = new ArrayList<DiscountItemRepresentation>();
+        //定义总的折扣价格
         BigDecimal totalDiscountPrice = new BigDecimal("0.00");
+        //定义优惠价格
         BigDecimal receivables = new BigDecimal("0.00");
+        //定义支付信息相关
         List<PaymentRepresentation> payments = new ArrayList<PaymentRepresentation>();
         List<PaymentCommand> frontPayments =  command.getPayments();
         for(PaymentCommand frontPayment:frontPayments){
@@ -78,33 +88,40 @@ public class OrderApp {
             String product =  orderItemCommand.getProduct();
             //用户购买的数量
             BigDecimal amount = orderItemCommand.getAmount();
-            //客户使用的优惠券
-
+            //根据产品编号获取产品信息
             ProductInfo productInfo = (ProductInfo) productInfoMap.get(product);
-
             //计算价格总合计
             BigDecimal priceSum = productInfo.getPrice().multiply(amount);
+
             totalPrice = totalPrice.add(priceSum);
             //客户如果使用了优惠券
+            String discountCard = null;
             if(discountCards != null && discountCards.size()>0){
-                String discountCard = discountCards.get(0);
-                //如果折扣不为空或者满减优惠不为空
-                if(isNotBlank(productInfo.getDiscount())|| isNotBlank(productInfo.getSubtractType())){
-                    DiscountItemRepresentation discountItemRepresentation = calculateDiscount(discountCard,amount,priceSum,productInfo);
-                    if(discountItemRepresentation.getDiscount().compareTo(BigDecimal.ZERO) > 0){
-                        totalDiscountPrice = totalDiscountPrice.add(discountItemRepresentation.getDiscount());
-                        discounts.add(discountItemRepresentation);
-                    }
+                 discountCard = discountCards.get(0);
+
+            }
+            //如果礼品折扣不为空或者满减优惠不为空
+            if(isNotBlank(productInfo.getDiscount())|| isNotBlank(productInfo.getSubtractType())){
+                DiscountItemRepresentation discountItemRepresentation = calculateDiscount(discountCard,amount,priceSum,productInfo);
+                //只保留优惠金额大于0的优惠信息
+                if(discountItemRepresentation.getDiscount().compareTo(BigDecimal.ZERO) > 0){
+                    totalDiscountPrice = totalDiscountPrice.add(discountItemRepresentation.getDiscount());
+                    discounts.add(discountItemRepresentation);
                 }
             }
             items.add(new OrderItemRepresentation(productInfo.getProduct(),  productInfo.getProductName(), productInfo.getPrice(),  amount, priceSum));
 
         }
+        //应收金额 = 总金额 - 优惠金额
         receivables = totalPrice.subtract(totalDiscountPrice);
+
+        //付款完成之后给用户调整积分信息
         member.addPointsIncreased(receivables);
+        //获取调整之后的用户的积分信息、新的客户等级
         int memberPoints = member.getMemberPoints();
         int memberPointsIncreased =  memberPoints - oldMemberPoints;
         String newMemberType = member.getMemberType();
+
         result = new OrderRepresentation(orderId,createTime,memberNo,memberName,oldMemberType,newMemberType,memberPointsIncreased,memberPoints,items,totalPrice,discounts,
                 totalDiscountPrice,receivables,payments,discountCards);
         return result;
@@ -180,6 +197,14 @@ public class OrderApp {
         return map;
     }
 
+    /**
+     * 计算商品优惠信息
+     * @param discountCard
+     * @param amount
+     * @param sumPrice
+     * @param productInfo
+     * @return
+     */
     public DiscountItemRepresentation calculateDiscount(String discountCard,BigDecimal amount,BigDecimal sumPrice,ProductInfo productInfo){
         BigDecimal maxDiscount = null;
         BigDecimal discountPrice = null;
@@ -191,7 +216,7 @@ public class OrderApp {
         discountMap.put("95折券",BigDecimal.valueOf(0.95));
         BigDecimal discount = discountMap.get(discountCard);
         List<BigDecimal> maxDiscountsList = new ArrayList<BigDecimal>();
-        if(discountCard.equals(productDiscount)){
+        if(isNotBlank(discountCard) && discountCard.equals(productDiscount)){
             discountPrice = sumPrice.multiply(new BigDecimal("1.00").subtract(discount));
             maxDiscountsList.add(discountPrice);
         }
@@ -207,6 +232,13 @@ public class OrderApp {
         return new DiscountItemRepresentation(productInfo.getProduct(),productInfo.getProductName(),maxDiscount);
     }
 
+    /**
+     * 比较是否满足满减条件 并返回 优惠金额
+     * @param sumPrice
+     * @param comparePrice
+     * @param subPrice
+     * @return
+     */
     public BigDecimal calculateSubTract(BigDecimal sumPrice,BigDecimal comparePrice,BigDecimal subPrice){
         BigDecimal result = new BigDecimal("0.00");
         int compareResult = comparePrice.compareTo(sumPrice); //compareResult = 1:comparePrice>sumPrice 0:comparePrice=sumPrice -1:comparePrice<sumPrice
@@ -215,6 +247,15 @@ public class OrderApp {
         }
         return result;
     }
+
+    /**
+     * 计算购买商品支持的最大优惠金额
+     * @param subTractType
+     * @param amount
+     * @param price
+     * @param sumPrice
+     * @return
+     */
     public BigDecimal calculateMaxSubTract(String subTractType,BigDecimal amount,BigDecimal price,BigDecimal sumPrice){
         BigDecimal maxDisCountPrice = null;
         String[] subTractTypes = subTractType.split(",");
@@ -260,6 +301,11 @@ public class OrderApp {
         return maxDisCountPrice;
     }
 
+    /**
+     * 判断参数不为空
+     * @param obejct
+     * @return
+     */
     public boolean isNotBlank(Object obejct){
         if(obejct != null && obejct != ""){
             return true;
@@ -267,12 +313,16 @@ public class OrderApp {
         return false;
     }
 
+    /**
+     * String 类型时间格式转date
+     * @param time
+     * @return
+     */
     public Date StringToDate(String time){
         Date date = null;
         try {
             SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-            date = sdf.parse(time); //创建时间
-
+            date = sdf.parse(time);
         } catch (ParseException e) {
             e.printStackTrace();
         }
