2db5d36ec6b119fe276ab9e29ab1fb471da585df
chenfeng <844474380@qq.com>
2019-07-02 22:33:35 +0800

    添加单元测试

--- src/test/java/com/coding/sales/OrderAppTest.java
+++ src/test/java/com/coding/sales/OrderAppTest.java
@@ -1,12 +1,16 @@
 package com.coding.sales;
 
+import com.coding.sales.entity.ProductInfo;
+import com.coding.sales.output.DiscountItemRepresentation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import java.io.File;
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Date;
 
 import static org.junit.Assert.assertEquals;
 
@@ -44,6 +48,29 @@ public class OrderAppTest {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource(fileName).getFile());
         return file.getAbsolutePath();
+    }
+
+    @Test
+    public void calculateDiscount() {
+        ProductInfo productInfo = (ProductInfo) ProductInfo.productInfoMap.get("001002");
+        assertEquals(new DiscountItemRepresentation("001002","2019北京世园会纪念银章大全40g",new BigDecimal("414.00")),
+                new OrderApp().calculateDiscount("9折券",new BigDecimal("3.000"),new BigDecimal("4140.000"),productInfo));
+
+    }
+
+    @Test
+    public void calculateSubTract() {
+        assertEquals(new BigDecimal("350"),new OrderApp().calculateSubTract(new BigDecimal("4000"),new BigDecimal("3000"),new BigDecimal("350")));
+    }
+
+    @Test
+    public void calculateMaxSubTract() {
+        assertEquals(new BigDecimal("350"),new OrderApp().calculateMaxSubTract("1,2,3",new BigDecimal("5.00"),new BigDecimal("698.00"),new BigDecimal("3490.00")));
+    }
+
+    @Test
+    public void isNotBlank() {
+        assertEquals(true,new OrderApp().isNotBlank("aaa"));
     }
 
 }
