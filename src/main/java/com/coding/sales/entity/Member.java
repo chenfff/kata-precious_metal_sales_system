a9d67b523c2b86c7ac898a6f235c401de6901575
loolin <10772332@qq.com>
2019-07-02 18:51:51 +0800

    会员信息添加初始化信息

--- src/main/java/com/coding/sales/entity/Member.java
+++ src/main/java/com/coding/sales/entity/Member.java
@@ -1,13 +1,37 @@
 package com.coding.sales.entity;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Member {
+    /**
+     * 初始化会员信息参数
+     */
+    public final static Map memberMap = new HashMap(){{
+        put("6236609999",new Member("马丁","6236609999",9860,"普卡"));
+        put("6630009999",new Member("王立","6630009999",48860,"金卡"));
+        put("8230009999",new Member("李想","8230009999",98860,"白金卡"));
+        put("9230009999",new Member("张三","9230009999",198860,"钻石卡")    );
+    }};
     private String memberName;
     private String memberCardNo;
     private int memberPoints;
     private String MemberType;
 
+    /**
+     * 会员信息
+     * @param memberName 会员姓名
+     * @param memberCardNo 会员卡号
+     * @param memberPoints 会员积分
+     * @param memberType 会员等级
+     */
+    public Member(String memberName, String memberCardNo, int memberPoints, String memberType) {
+        this.memberName = memberName;
+        this.memberCardNo = memberCardNo;
+        this.memberPoints = memberPoints;
+        MemberType = memberType;
+    }
 
     public String getMemberName() {
         return memberName;
